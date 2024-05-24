d3ded5df3159c51f167f1d1475cfd265f5223818
Jens Astrup <jensaiden@gmail.com>
2024-05-24 12:06:43 -0400

    Use own eslint rules for project, add type ordering rule

--- eslint-config-base.js
+++ eslint-config-base.js
@@ -4,23 +4,27 @@ const baseConfig = {
     'plugin:@typescript-eslint/recommended',
     'plugin:perfectionist/recommended-natural',
     'plugin:import/warnings',
-    'plugin:import/typescript'
+    'plugin:import/typescript',
   ],
   plugins: [
     '@typescript-eslint',
     'perfectionist',
-    'import'
+    'import',
+    'no-loops',
+    'type-ordering'
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2020,
-    "sourceType": "module"
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2020,
+    'sourceType': 'module'
   },
   rules: {
+    'type-ordering/type-ordering': 'error',
     'dot-notation': 'error',
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'no-console': ['error', {'allow': ['error', 'warn']}],
+    'no-loops/no-loops': 'error',
     'no-magic-numbers': ['error', {'ignoreArrayIndexes': true, 'ignore': [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}],
     'no-use-before-define': 'off',
     'quotes': ['error', 'single'],
@@ -31,18 +35,18 @@ const baseConfig = {
     'perfectionist/sort-classes': 'off',
     'perfectionist/sort-interfaces': 'off',
     'perfectionist/sort-objects': 'off',
-    "perfectionist/sort-object-types": "off",
-    "padding-line-between-statements": [
-      "error",
-      { "blankLine": "always", "prev": "function", "next": "*" },
-      { "blankLine": "always", "prev": "*", "next": "function" },
+    'perfectionist/sort-object-types': 'off',
+    'padding-line-between-statements': [
+      'error',
+      { 'blankLine': 'always', 'prev': 'function', 'next': '*' },
+      { 'blankLine': 'always', 'prev': '*', 'next': 'function' },
     ],
     'import/order': [
       'error',
       {
         groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
         pathGroupsExcludedImportTypes: ['builtin'],
-        "newlines-between": 'always',
+        'newlines-between': 'always',
         alphabetize: {
           order: 'asc',
           caseInsensitive: true
@@ -50,7 +54,15 @@ const baseConfig = {
       }
     ],
     'import/newline-after-import': ['error', { count: 2 }]
-  }
-};
+  },
+  overrides: [
+    {
+      'files': ['test/**'],
+      'plugins': ['jest'],
+      'extends': ['plugin:jest/recommended'],
+      'rules': { }
+    }
+  ]
+}
 
-module.exports = baseConfig;
+module.exports = baseConfig
