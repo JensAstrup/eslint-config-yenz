cf701772757ee0e04f105a0273454837fa2a866d
Jens Astrup <jensaiden@gmail.com>
2024-05-27 16:18:45 -0400

    Update yenz config, improve styling

--- eslint-config-base.js
+++ eslint-config-base.js
@@ -14,24 +14,24 @@ const baseConfig = {
     '@stylistic',
     'perfectionist',
     'import',
-    'no-loops',
     'yenz',
     'jest'
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 2020,
-    'sourceType': 'module',
-    'project': './tsconfig.json'
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   rules: {
     'yenz/type-ordering': 'error',
+    'yenz/no-loops': 'error',
     'dot-notation': 'error',
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    'no-console': ['error', {'allow': ['error', 'warn']}],
+    'no-console': ['error', { allow: ['error', 'warn'] }],
     'no-loops/no-loops': 'error',
-    'no-magic-numbers': ['error', {'ignoreArrayIndexes': true, 'ignore': [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}],
+    'no-magic-numbers': ['error', { ignoreArrayIndexes: true, ignore: [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }],
     'no-use-before-define': 'off',
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
@@ -54,16 +54,16 @@ const baseConfig = {
     '@stylistic/indent': 'off',
     'padding-line-between-statements': [
       'error',
-      { 'blankLine': 'always', 'prev': 'function', 'next': '*' },
-      { 'blankLine': 'always', 'prev': '*', 'next': 'function' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'function' },
     ],
     'import/order': [
       'error',
       {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        pathGroupsExcludedImportTypes: ['builtin'],
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'pathGroupsExcludedImportTypes': ['builtin'],
         'newlines-between': 'always',
-        alphabetize: {
+        'alphabetize': {
           order: 'asc',
           caseInsensitive: true
         }
@@ -73,10 +73,10 @@ const baseConfig = {
   },
   overrides: [
     {
-      'files': ['test/**'],
-      'plugins': ['jest'],
-      'extends': ['plugin:jest/recommended', 'plugin:jest/style'],
-      'rules': {
+      files: ['test/**'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+      rules: {
         'jest/prefer-called-with': 'error',
         'jest/prefer-equality-matcher': 'error',
         'jest/prefer-spy-on': 'error'
