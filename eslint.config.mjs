cccb46c2c6d4f8e51043eeb9dee74204386d448d
Jens Astrup <jensaiden@gmail.com>
2024-11-28 12:06:17 -0500

    Warn on type safety rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -59,11 +59,11 @@ export default [...fixupConfigRules(compat.extends(
   '@typescript-eslint/no-floating-promises': 'off',
   '@typescript-eslint/no-misused-promises': 'off',
   '@typescript-eslint/no-unnecessary-condition': 'error',
-  '@typescript-eslint/no-unsafe-argument': 'off',
-  '@typescript-eslint/no-unsafe-assignment': 'off',
-  '@typescript-eslint/no-unsafe-call': 'off',
-  '@typescript-eslint/no-unsafe-member-access': 'off',
-  '@typescript-eslint/no-unsafe-return': 'off',
+  '@typescript-eslint/no-unsafe-argument': 'warn',
+  '@typescript-eslint/no-unsafe-assignment': 'warn',
+  '@typescript-eslint/no-unsafe-call': 'warn',
+  '@typescript-eslint/no-unsafe-member-access': 'warn',
+  '@typescript-eslint/no-unsafe-return': 'warn',
   '@typescript-eslint/no-unused-vars': ['error', {
     varsIgnorePattern: '^_$',
     argsIgnorePattern: '^_$'
