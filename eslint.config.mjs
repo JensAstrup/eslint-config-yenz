ef35157d7f3aba0fa37bb90d49eacb214ffb6d04
Jens Astrup <jensaiden@gmail.com>
2024-11-28 11:54:39 -0500

    Upgrade dependencies for eslint, move eslint and typescript to peerdependencies, turned off new rule added in version update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -50,84 +50,79 @@ export default [...fixupConfigRules(compat.extends(
   },
 
   rules: {
-    'yenz/type-ordering': 'error',
-    'yenz/no-loops': 'error',
-    'dot-notation': 'error',
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    'no-case-declarations': 'off',
-
-    'no-console': ['error', {
-      allow: ['error', 'warn']
-    }],
-
-    'no-magic-numbers': ['error', {
-      ignoreArrayIndexes: true,
-      ignore: [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    }],
-
-    'no-use-before-define': 'off',
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'brace-style': ['error', 'stroustrup'],
-    'perfectionist/sort-enums': 'off',
-    'perfectionist/sort-imports': 'off',
-    'perfectionist/sort-named-exports': 'off',
-    'perfectionist/sort-named-imports': 'off',
-    'perfectionist/sort-union-types': 'off',
-    'perfectionist/sort-classes': 'off',
-    'perfectionist/sort-interfaces': 'off',
-    'perfectionist/sort-objects': 'off',
-    'perfectionist/sort-object-types': 'off',
-
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/no-confusing-void-expression': 'error',
-    '@typescript-eslint/no-unnecessary-condition': 'error',
-    '@typescript-eslint/prefer-find': 'error',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-misused-promises': 'off',
-    '@typescript-eslint/unbound-method': 'off',
-    '@stylistic/no-multiple-empty-lines': 'off',
-    '@stylistic/comma-dangle': 'off',
-    '@stylistic/indent': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-argument': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', {
-      varsIgnorePattern: '^_$',
-      argsIgnorePattern: '^_$'
-    }],
-
-    'padding-line-between-statements': ['error', {
-      blankLine: 'always',
-      prev: 'function',
-      next: '*'
-    }, {
-      blankLine: 'always',
-      prev: '*',
-      next: 'function'
-    }],
-
-    'import/no-named-as-default': 'off',
-
-    'import/order': ['error', {
-      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-      pathGroupsExcludedImportTypes: ['builtin'],
-      'newlines-between': 'always',
-
-      alphabetize: {
-        order: 'asc',
-        caseInsensitive: true
-      }
-    }],
-
-    'import/newline-after-import': ['error', {
-      count: 2
-    }]
-  }
+  '@stylistic/comma-dangle': 'off',
+  '@stylistic/indent': 'off',
+  '@stylistic/no-multiple-empty-lines': 'off',
+  '@typescript-eslint/explicit-function-return-type': 'error',
+  '@typescript-eslint/explicit-member-accessibility': 'off',
+  '@typescript-eslint/no-confusing-void-expression': 'error',
+  // It can be useful to have an empty object type when simplifying types
+  // ex. export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  '@typescript-eslint/no-empty-object-type': 'off',
+  '@typescript-eslint/no-floating-promises': 'off',
+  '@typescript-eslint/no-misused-promises': 'off',
+  '@typescript-eslint/no-unnecessary-condition': 'error',
+  '@typescript-eslint/no-unsafe-argument': 'off',
+  '@typescript-eslint/no-unsafe-assignment': 'off',
+  '@typescript-eslint/no-unsafe-call': 'off',
+  '@typescript-eslint/no-unsafe-member-access': 'off',
+  '@typescript-eslint/no-unsafe-return': 'off',
+  '@typescript-eslint/no-unused-vars': ['error', {
+    varsIgnorePattern: '^_$',
+    argsIgnorePattern: '^_$'
+  }],
+  '@typescript-eslint/prefer-find': 'warn',
+  '@typescript-eslint/unbound-method': 'off',
+  'brace-style': ['error', 'stroustrup'],
+  'dot-notation': 'error',
+  'import/newline-after-import': ['error', {
+    count: 2
+  }],
+  'import/no-named-as-default': 'off',
+  'import/order': ['error', {
+    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+    pathGroupsExcludedImportTypes: ['builtin'],
+    'newlines-between': 'always',
+    alphabetize: {
+      order: 'asc',
+      caseInsensitive: true
+    }
+  }],
+  indent: ['error', 2],
+  'linebreak-style': ['error', 'unix'],
+  'no-case-declarations': 'off',
+  'no-console': ['error', {
+    allow: ['error', 'warn']
+  }],
+  'no-loops': 'error',
+  'no-magic-numbers': ['error', {
+    ignoreArrayIndexes: true,
+    ignore: [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  }],
+  'no-use-before-define': 'off',
+  'padding-line-between-statements': ['error', {
+    blankLine: 'always',
+    prev: 'function',
+    next: '*'
+  }, {
+    blankLine: 'always',
+    prev: '*',
+    next: 'function'
+  }],
+  'perfectionist/sort-classes': 'off',
+  'perfectionist/sort-enums': 'off',
+  'perfectionist/sort-imports': 'off',
+  'perfectionist/sort-interfaces': 'off',
+  'perfectionist/sort-named-exports': 'off',
+  'perfectionist/sort-named-imports': 'off',
+  'perfectionist/sort-object-types': 'off',
+  'perfectionist/sort-objects': 'off',
+  'perfectionist/sort-union-types': 'off',
+  quotes: ['error', 'single'],
+  semi: ['error', 'never'],
+  'yenz/no-loops': 'error',
+  'yenz/type-ordering': 'error'
+}
 }, ...fixupConfigRules(compat.extends('plugin:jest/recommended', 'plugin:jest/style')).map(config => ({
   ...config,
   files: ['test/**']
